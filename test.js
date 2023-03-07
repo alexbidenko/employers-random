a5da2fd575a52f6c5f85260007891cc63a0a80b0
Alexander <alexbidenko1998@gmail.com>
2023-03-07 17:20:29 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,31 @@
+clear()
+
+const MAX_UINT32_VALUE = 4294967295; // 2 ** 32 - 1
+
+const getRandomValue = (max) => {
+  const value = crypto.getRandomValues(new Uint32Array(1))[0] / MAX_UINT32_VALUE;
+
+  if (!max || max === 1) return value;
+
+  return Math.ceil(value * max);
+};
+
+const getRandomList = (list, count) => {
+  let similarCount = count;
+
+  const result = new Array(similarCount);
+  let len = list.length;
+  const taken = new Array(len);
+
+  while (similarCount--) {
+    const x = Math.floor(getRandomValue() * len);
+    result[similarCount] = list[x in taken ? taken[x] : x];
+    taken[x] = --len in taken ? taken[len] : len;
+  }
+  return result;
+};
+
+
+const employers = [...document.querySelectorAll('[role=list][aria-label=Participants] [role=listitem] [avatar-tooltip-id] [jsname] span')].map((el) => el.innerHTML);
+
+getRandomList(employers, 10);
